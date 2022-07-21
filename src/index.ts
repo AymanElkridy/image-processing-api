4b18842cd82751037dc0ede15a676e7e6f41b740
AymanElkridy <aymanelkridy@gmail.com>
2022-07-21 18:38:03 +0200

    applied prettier

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ import sharp from 'sharp';
 
 const app = express();
 const port = 3002;
-const listener = ():void => {console.log(`server running on port: ${port}`);};
+const listener = (): void => {
+  console.log(`server running on port: ${port}`);
+};
 
 // GLOBAL CONSTANTS
 
@@ -19,15 +21,16 @@ const thumbDir = './assets/images/thumbnails/';
 // IMAGE RESIZING GET REQUEST
 
 app.get(imageEndPoint, async (req, res) => {
-  
   // Declaring variables
-  let filename = '', width = 0, height = 0;
+  let filename = '',
+    width = 0,
+    height = 0;
 
   // Getting filename
   if (!req.query.filename) {
     res.status(400).end('Error: filename is not specified');
   } else {
-    filename = (req.query.filename as unknown) as string;
+    filename = req.query.filename as unknown as string;
 
     // Getting width
     if (!req.query.width) {
@@ -37,7 +40,7 @@ app.get(imageEndPoint, async (req, res) => {
     } else if (Number(req.query.width) === 0) {
       res.status(400).end('Error: width must be greater than 0');
     } else {
-      width = (req.query.width as unknown) as number;
+      width = req.query.width as unknown as number;
 
       // Getting height
       if (!req.query.height) {
@@ -47,7 +50,7 @@ app.get(imageEndPoint, async (req, res) => {
       } else if (Number(req.query.height) === 0) {
         res.status(400).end('Error: height must be greater than 0');
       } else {
-        height = (req.query.height as unknown) as number;
+        height = req.query.height as unknown as number;
       }
     }
   }
@@ -55,17 +58,24 @@ app.get(imageEndPoint, async (req, res) => {
   // Checking if parameters are correct
   if (filename && width && height) {
     const thumbPath = `${thumbDir}${filename}-${width}-${height}.jpg`;
-    try { // Checking if image is resized before
+    try {
+      // Checking if image is resized before
       const thumb = await fs.readFile(thumbPath);
       res.status(200).end(thumb);
     } catch {
-      try { // Resizing image, saving it, then rendering it
+      try {
+        // Grabbing image
         const img = await fs.readFile(`${imageDir}${filename}.jpg`);
         try {
-          await sharp(img).resize(Number(width), Number(height)).toFile(thumbPath);
+          // Resizing image
+          await sharp(img)
+            .resize(Number(width), Number(height))
+            .toFile(thumbPath);
+          // Rendering resized image
           const thumb = await fs.readFile(thumbPath);
           res.status(200).end(thumb);
-        } catch (err) { // Catching processing file error
+        } catch (err) {
+          // Catching processing file error
           console.error(err);
           res.status(400).end('Error: Failed to process file');
         }
