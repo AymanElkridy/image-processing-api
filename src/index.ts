3ad3a5b80a37c7cb7732697db19c5e1189677353
AymanElkridy <aymanelkridy@gmail.com>
2022-07-21 16:01:35 +0200

    quick commit

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ const listener = ():void => {console.log(`server running on port: ${port}`);}
 
 // GLOBAL CONSTANTS
 
-const baseURL = `localhost:${port}`;
-const imageEndPoint = '/image';
-const imageDir = './src/images/';
-const thumbDir = './src/images/thumbnails/';
+const baseURL:string = `localhost:${port}`;
+const imageEndPoint:string = '/image';
+const imageDir:string = './src/images/';
+const thumbDir:string = './src/images/thumbnails/';
 
 // IMAGE RESIZING GET REQUEST
 
@@ -26,27 +26,27 @@ app.get(imageEndPoint, async (req, res) => {
 
   // Getting filename
   if (!req.query.filename) {
-    res.send('Error: filename is not specified');
+    res.status(400).end('Error: filename is not specified');
   } else {
     filename = (req.query.filename as unknown) as string;
 
     // Getting width
     if (!req.query.width) {
-      res.send('Error: width is not specified');
+      res.status(400).end('Error: width is not specified');
     } else if (isNaN(Number(req.query.width))) {
-      res.send('Error: width must be a number');
+      res.status(400).end('Error: width must be a number');
     } else if (Number(req.query.width) === 0) {
-      res.send('Error: width must be greater than 0');
+      res.status(400).end('Error: width must be greater than 0');
     } else {
       width = (req.query.width as unknown) as number;
 
       // Getting height
       if (!req.query.height) {
-        res.send('Error: height is not specified');
+        res.status(400).end('Error: height is not specified');
       } else if (isNaN(Number(req.query.height))) {
-        res.send('Error: height must be a number');
+        res.status(400).end('Error: height must be a number');
       } else if (Number(req.query.height) === 0) {
-        res.send('Error: height must be greater than 0');
+        res.status(400).end('Error: height must be greater than 0');
       } else {
         height = (req.query.height as unknown) as number;
       }
@@ -58,13 +58,13 @@ app.get(imageEndPoint, async (req, res) => {
     let thumbPath = `${thumbDir}${filename}-${width}-${height}.jpg`;
     try { // Checking if image is resized before
       const thumb = await fs.readFile(thumbPath);
-      res.end(thumb);
+      res.status(200).end(thumb);
     } catch {
       try { // Resizing image, saving it, then rendering it
         const img = await fs.readFile(`${imageDir}${filename}.jpg`);
         await sharp(img).resize(Number(width), Number(height)).toFile(thumbPath);
         const thumb = await fs.readFile(thumbPath);
-        res.end(thumb);
+        res.status(200).end(thumb);
       } catch (err) { // Catching processing file error
         console.error(err);
         res.status(400).end('Error: Failed to process file');
